c7f2334029eb2bef9f64b417e6a282b44ead36ca
james-cain <m15960192140@163.com>
2018-07-26 01:39:30 +0800

    feat: 增加pc端物料模板

---
+++ template/src/__className__.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="content-container {{name}}">
+    <h1>{{className}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '{{className}}',
+
+  data() {
+    return {}
+  },
+
+  created() {},
+
+  methods: {}
+}
+
+</script>
+
+<style scoped>
+  .content-container {
+    background: #fff;
+    overflow: auto;
+  }
+  .{{name}} {
+
+  }
+</style>
